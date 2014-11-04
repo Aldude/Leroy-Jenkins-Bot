a29ddcd24128297d4dc69e485abd2db155437de0
Blake Sakaluk <blake.sakaluk@gmail.com>
2014-11-04 14:16:30 -0700

    Testing 12 Zerglings vs 10

--- UAlbertaBot/Source/StrategyManager.cpp
+++ UAlbertaBot/Source/StrategyManager.cpp
@@ -34,7 +34,7 @@ void StrategyManager::addStrategies()
 	
 	terranOpeningBook[TerranMarineRush]		= "0 0 0 0 0 1 0 0 3 0 0 3 0 1 0 4 0 0 0 6";
 	
-	zergOpeningBook[ZergZerglingRush]		= "3 4 4 4 4 4 0 0 1";
+	zergOpeningBook[ZergZerglingRush]		= "3 4 4 4 4 4 4 0 0 1";
 
 	if (selfRace == BWAPI::Races::Protoss)
 	{
