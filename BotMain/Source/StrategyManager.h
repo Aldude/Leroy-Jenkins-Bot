c2d78075b637439153acf57ae81ebb95a3e99076
Blake Sakaluk <blake.sakaluk@gmail.com>
2014-11-12 17:21:08 -0700

    Minor cleanup of some code, renamed stuff

---
+++ BotMain/Source/StrategyManager.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "Common.h"
+#include "BWTA.h"
+#include "base/BuildOrderQueue.h"
+#include "InformationManager.h"
+#include "base/WorkerManager.h"
+#include "base/StarcraftBuildOrderSearchManager.h"
+#include <sys/stat.h>
+#include <cstdlib>
+
+#include "..\..\StarcraftBuildOrderSearch\Source\starcraftsearch\StarcraftData.hpp"
+
+typedef std::pair<int, int> IntPair;
+typedef std::pair<MetaType, UnitCountType> MetaPair;
+typedef std::vector<MetaPair> MetaPairVector;
+
+class StrategyManager 
+{
+	StrategyManager();
+	~StrategyManager() {}
+
+	std::vector<std::string>	zergOpeningBook;
+
+	std::string					readDir;
+	std::string					writeDir;
+	std::vector<IntPair>		results;
+	std::vector<int>			usableStrategies;
+	int							currentStrategy;
+
+	BWAPI::Race					selfRace;
+	BWAPI::Race					enemyRace;
+
+	bool						firstAttackSent;
+
+	void	addStrategies();
+	void	setStrategy();
+	void	readResults();
+	void	writeResults();
+
+	const	int					getScore(BWAPI::Player * player) const;
+	const	double				getUCBValue(const size_t & strategy) const;
+
+	const	bool				expandZergZerglingRush() const;
+	const	MetaPairVector		getZergBuildOrderGoal() const;
+
+public:
+
+	enum { ZergZerglingRush=0, ZergZerglingRush2=1, NumZergStrategies=2 };
+
+	static	StrategyManager &	Instance();
+
+			void				onEnd(const bool isWinner);
+	
+	const	bool				regroup(int numInRadius);
+	const	bool				doAttack(const std::set<BWAPI::Unit *> & freeUnits);
+	const	int				    defendWithWorkers();
+	const	bool				rushDetected();
+
+	const	int					getCurrentStrategy();
+
+	const	MetaPairVector		getBuildOrderGoal();
+	const	std::string			getOpeningBook() const;
+};
